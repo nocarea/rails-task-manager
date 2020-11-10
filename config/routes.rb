1b5c05df9e506226cfdc9a3c732d3289194e4eb1
Marius Moeller <viktor.rellom@protonmail.com>
2020-11-10 16:24:04 +0100

    done

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  get "all", to: "tasks#all"
+  get "tasks", to: "tasks#index"
+  get "tasks/new", to: "tasks#new", as: :new
+  post "tasks", to: "tasks#create", as: :create
+  get "tasks/:id", to: "tasks#show", as: :task
+  get "tasks/:id/edit", to: "tasks#edit", as: :edit
+  patch "tasks/:id", to: "tasks#update", as: :update
+  delete "tasks/:id", to: "tasks#destroy", as: :destroy
 end
